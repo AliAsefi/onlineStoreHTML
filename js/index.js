46f6d8519f7a92873a16af853af56f0c5e53c040
AliAsefi <asefi.alireza64@gmail.com>
2025-03-24 21:19:53 +0100

    index completed

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
+let totalCartQuantity = 0;
+
 // Fetch products from the backend API
 document.addEventListener('DOMContentLoaded', async()=> { 
     
-    const productContainer = document.getElementById('product-container');
+    const productContainer = document.getElementById('products-grid');
 
   try {
     const products = await fetchData("products");
@@ -9,15 +11,83 @@ document.addEventListener('DOMContentLoaded', async()=> {
     products.forEach(product => {
         const productDiv = document.createElement('div');
         productDiv.classList.add('product');
+
         productDiv.innerHTML = `
+          <div class="product-image-container">
+            <img class="product-image" src="${product.image}">
+          </div>
+          <div class="product-name">
             <h3>${product.name}</h3>
+          </div>
+          <div class="product-description">
+            <button class="descriptionBtn" onclick="toggleDescription(this)">Show Description</button>
+            <p class="description" style="display: none">${product.description}</p>
+          </div>
+          <div class="product-price">
             <p>Price: $${product.price}</p>
-            <p>${product.description}</p>
+          </div>
+          <div class="product-discountPercentage">
+            <p>Discount: %${product.discountPercentage}</p>
+          </div>
+          <div class="product-quantity-container">
+            Quantity: <select class="quantity-select">
+              <option selected value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+              <option value="6">6</option>
+              <option value="7">7</option>
+              <option value="8">8</option>
+              <option value="9">9</option>
+              <option value="10">10</option>
+            </select>
+          </div>
+
+          <div class="addToCart">
+            <button class="addToCartBtn jsAddToCartBtn" 
+            data-product-id="${product.id}">
+            Add to cart
+            </button>
+          </div>
         `;
+
         productContainer.appendChild(productDiv);
+
+        /*
+          ------------------------------------------
+          update cart quantity:
+          1.Listen to all "Add to Cart" buttons' clicks.
+          2.Get the selected quantity for that product.
+          3.Keep track of total items added to the cart.
+          4.Update .cart-quantity div.
+        */
+        const addToCartBtn = productDiv.querySelector('.addToCartBtn');
+        const quantitySelect = productDiv.querySelector('.quantity-select');
+
+        addToCartBtn.addEventListener('click',()=>{
+          const quantity = parseInt(quantitySelect.value);
+          addToCartBtn.textContent='Added';
+          totalCartQuantity += quantity;
+
+          const cartQuantity = document.querySelector('.cart-quantity');
+          cartQuantity.textContent = totalCartQuantity;
+        })
     });
 
   } catch (error) {
     console.error('Error fetching products:', error);
   }
 });
+
+function toggleDescription(button){
+  const description = button.nextElementSibling;
+
+  if(description.style.display == 'none'){
+    description.style.display = "block";
+    button.textContent = "Hide Description"; 
+  }else{
+    description.style.display = "none";
+    button.textContent = "Show Description"; 
+  }
+}
